422d6e3ab60bbe08fe58a7e5df177a7e98d0b808
Josh Jancula <josh@jancula.com>
2018-01-13 04:40:58 +0000

    trying on to deploy

---
+++ src/components/CharacterCard/CharacterCard.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./CharacterCard.css";
+
+class CharacterCard extends Component  {
+    
+    state = {
+    clicked: false
+  };
+    
+ // when you click on an image
+    handleClickTrue = event => {
+    
+    if(this.state.clicked === true) {// game is now over
+      // update the display
+      this.props.changeDisplayLoser();
+      // reset the score
+      this.props.resetScore();
+    } else { 
+     // update the display
+      this.props.changeDisplayCorrect();
+      // update the status of "clicked" to true
+      this.setState({ clicked: true });
+      // shuffle the images around
+      this.props.shuffle();
+      // increase the score by one
+      this.props.setScore();
+     
+    }
+    }; 
+    
+    
+    render() {
+        return (
+              <div className="card">
+              <img alt={this.props.id} src={this.props.image} onClick={() => this.handleClickTrue(this.props.id)} />
+    </div>
+            
+            );
+    }
+    
+}
+
+export default CharacterCard;
